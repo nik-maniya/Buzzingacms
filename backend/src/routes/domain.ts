65012b8b9d19689a654788cfa42aa0a5cc62b330
Mithil-ux <mithil.m@buzzinga.design>
2025-11-13 14:43:26 +0530

    mithil/minor_changes

--- backend/src/routes/domain.ts
+++ backend/src/routes/domain.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { upsertDomain } from '../controller/domainController';
+import { createDNSRecord, getDomain, upsertDomain } from '../controller/domainController';
 import { authenticate } from '../middleware/auth';
 
 const router = Router();
 
 // POST /api/domain - Upsert domain
 router.post('/upsertDomain', authenticate, upsertDomain);
+
+// GET /api/domain - Get domain
+router.get('/getDomain/:domainName', authenticate, getDomain);
+
+// POST /api/domain/createDNSRecord - Create DNS record
+router.post('/createDNSRecord', authenticate, createDNSRecord);
 
 export default router;
